649d7b5f2debc12dbc95d0e1d43eb2bc7818dad7
Cristian Mamani <cristian.mamani@mojix.com>
2018-01-29 21:12:32 -0400

    Ejemplo de incrementales

--- src/bo/edu/umss/cs/ceis/Main.java
+++ src/bo/edu/umss/cs/ceis/Main.java
@@ -3,6 +3,11 @@ package bo.edu.umss.cs.ceis;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Bienvenidos al curso de Java UMSS!!!");
+        System.out.println("Bienvenidos UMSS!!!");
+
+        int x = 3;
+
+        System.out.println(x++);
+        System.out.println(x);
     }
 }
