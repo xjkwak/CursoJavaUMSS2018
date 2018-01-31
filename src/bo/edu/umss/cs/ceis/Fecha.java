c558bbaf60f94de55fa41d41dee92c86a09c81cf
Cristian Mamani <cristian.mamani@mojix.com>
2018-01-30 21:24:05 -0400

    Ejemplo clase Fecha

--- src/bo/edu/umss/cs/ceis/Fecha.java
+++ src/bo/edu/umss/cs/ceis/Fecha.java
@@ -1,4 +1,18 @@
 package bo.edu.umss.cs.ceis;
 
 public class Fecha {
+    int dia;
+    int mes;
+    int año;
+
+    public Fecha(int a, int b, int c) {
+        dia = a;
+        mes = b;
+        año = c;
+    }
+
+    public void mostrarFechaCorta() {
+        String corta = dia + "/" + mes + "/" + año;
+        System.out.println(corta);
+    }
 }
